1a5c491c67632c9f1adb4195c6aa64c3d4de737e
Kristian Rosenvold <kristian.rosenvold@nrk.no>
2019-04-08 08:33:11 +0200

    Added relations

---
+++ mdb/client/relations.py
@@ -0,0 +1,6 @@
+EO_SUBJECTS = "http://id.nrk.no/2016/mdb/relation/subjects"
+EO_CATEGORIES = "http://id.nrk.no/2016/mdb/relation/categories"
+EO_LOCATIONS= "http://id.nrk.no/2016/mdb/relation/locations"
+EO_SPATIALS = "http://id.nrk.no/2016/mdb/relation/spatials"
+EO_CONTRIBUTORS = "http://id.nrk.no/2016/mdb/relation/contributors"
+REFERENCES = "http://id.nrk.no/2016/mdb/relation/references"
