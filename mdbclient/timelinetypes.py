6e1d510ac0c9269f71ca83ef50c32389475a7864
Kristian Rosenvold <kristian.rosenvold@nrk.no>
2019-10-11 09:41:08 +0200

    Added timelinetypes

---
+++ mdbclient/timelinetypes.py
@@ -0,0 +1,6 @@
+GENEALOGY_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Genealogy"
+RIGHTS_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Rights"
+INTERNAL_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Internal"
+TECHNICAL_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Technical"
+INDEXPOINTS_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/IndexPoints"
+
