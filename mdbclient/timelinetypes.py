58b5b10d0521cb7edfc6f4b1e0d6a8366ac8beda
Kristian Rosenvold <kristian.rosenvold@nrk.no>
2019-10-17 08:30:49 +0200

    ADded item types

--- mdbclient/timelinetypes.py
+++ mdbclient/timelinetypes.py
@@ -1,6 +1,11 @@
-GENEALOGY_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Genealogy"
-RIGHTS_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Rights"
-INTERNAL_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Internal"
-TECHNICAL_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/Technical"
-INDEXPOINTS_TIMELINE="http://id.nrk.no/2017/mdb/timelinetype/IndexPoints"
-
+GENEALOGY_TIMELINE = "http://id.nrk.no/2017/mdb/timelinetype/Genealogy"
+RIGHTS_TIMELINE = "http://id.nrk.no/2017/mdb/timelinetype/Rights"
+INTERNAL_TIMELINE = "http://id.nrk.no/2017/mdb/timelinetype/Internal"
+TECHNICAL_TIMELINE = "http://id.nrk.no/2017/mdb/timelinetype/Technical"
+INDEXPOINTS_TIMELINE = "http://id.nrk.no/2017/mdb/timelinetype/IndexPoints"
+TIMELINE_ITEMTYPE_EXTRACTEDVERSIONTIMELINEITEM = 'http://id.nrk.no/2017/mdb/timelineitem/ExtractedVersionTimelineItem'
+TIMELINE_ITEMTYPE_EXPLOITATIONISSUETIMELINEITEM = 'http://id.nrk.no/2017/mdb/timelineitem/ExploitationIssueTimelineItem'
+TIMELINE_ITEMTYPE_GENERALRIGHTS = 'http://id.nrk.no/2017/mdb/timelineitem/GeneralRightsTimelineItem'
+TIMELINE_ITEMTYPE_INDEXPOINTTIMELINEITEM = 'http://id.nrk.no/2017/mdb/timelineitem/IndexpointTimelineItem'
+TIMELINE_ITEMTYPE_INTERNALTIMELINEITEM = 'http://id.nrk.no/2017/mdb/timelineitem/InternalTimelineItem'
+TIMELINE_ITEMTYPE_TECHNICALTIMELINEITEM = 'http://id.nrk.no/2017/mdb/timelineitem/TechnicalTimelineItem'
